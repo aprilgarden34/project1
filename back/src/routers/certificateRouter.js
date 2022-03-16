fdd64b12f624870a5622839aa5e888219eccf97c
Heyden <nhm6947@gmail.com>
2022-03-16 16:57:59 +0900

    Feat: certificateRouter 기초 뼈대 작성

---
+++ back/src/routers/certificateRouter.js
@@ -0,0 +1,128 @@
+import is from "@sindresorhus/is";
+import { Router } from "express";
+import { login_required } from "../middlewares/login_required";
+import { userAuthService } from "../services/userService";
+import { certificateService } from "../services/certificateService";
+
+const certificateRouter = Router();
+
+// 나의 자격증 생성
+certificateRouter.post("/certificate/create", async function (req, res, next) {
+  try {
+    // req (request) 에서 데이터 가져오기
+    const certificationName = req.body.certificationName;
+    const certificationDesc = req.body.certificationDesc;
+    const certificationDate = req.body.certificationDate;
+
+    // 위 데이터를 이용하여 자격증 db에서 유저 찾기
+    const newCertification = await certificateService.addCertification({ 
+        certificationName,
+        certificationDesc,
+        certificationDate,
+    });
+
+    if (newCertification.errorMessage) {
+      throw new Error(newCertification.errorMessage);
+    }
+
+    res.status(201).json(newCertification);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 토큰발급용
+certificateRouter.post("/user/login", async function (req, res, next) {
+  try {
+    // req (request) 에서 데이터 가져오기
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // 위 데이터를 이용하여 유저 db에서 유저 찾기
+    const user = await userAuthService.getUser({ email, password });
+
+    if (user.errorMessage) {
+      throw new Error(user.errorMessage);
+    }
+
+    res.status(200).send(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 나의 자격증 조회 
+certificateRouter.get(
+  "/certificates/:id",
+  login_required,
+  async function (req, res, next) {
+    try {
+      // 전체 사용자 목록을 얻음
+      const users = await userAuthService.getUsers();
+      res.status(200).send(users);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// 나의 자격증 수정
+certificateRouter.put(
+  "/certificates/:id",
+  login_required,
+  async function (req, res, next) {
+    try {
+      // URI로부터 사용자 id를 추출함.
+      const user_id = req.params.id;
+      // body data 로부터 업데이트할 사용자 정보를 추출함.
+      const name = req.body.name ?? null;
+      const email = req.body.email ?? null;
+      const password = req.body.password ?? null;
+      const description = req.body.description ?? null;
+
+      const toUpdate = { name, email, password, description };
+
+      // 해당 사용자 아이디로 사용자 정보를 db에서 찾아 업데이트함. 업데이트 요소가 없을 시 생략함
+      const updatedUser = await userAuthService.setUser({ user_id, toUpdate });
+
+      if (updatedUser.errorMessage) {
+        throw new Error(updatedUser.errorMessage);
+      }
+
+      res.status(200).json(updatedUser);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// 네트워크의 특정 유저 자격증 조회
+certificateRouter.get(
+  "/certificatelist/:user_id",
+  login_required,
+  async function (req, res, next) {
+    try {
+      const user_id = req.params.id;
+      const currentUserInfo = await userAuthService.getUserInfo({ user_id });
+
+      if (currentUserInfo.errorMessage) {
+        throw new Error(currentUserInfo.errorMessage);
+      }
+
+      res.status(200).send(currentUserInfo);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// jwt 토큰 기능 확인용, 삭제해도 되는 라우터임.
+certificateRouter.get("/afterlogin", login_required, function (req, res, next) {
+  res
+    .status(200)
+    .send(
+      `안녕하세요 ${req.currentUserId}님, jwt 웹 토큰 기능 정상 작동 중입니다.`
+    );
+});
+
+export { certificateRouter };
