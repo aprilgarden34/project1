9493459ece53d922d669e756e5c8f3d5265d6d18
GeonTae Park <rssr96@naver.com>
2022-03-18 13:09:40 +0900

    Feat: 에러처리추가

--- back/src/routers/educationRouter.js
+++ back/src/routers/educationRouter.js
@@ -8,24 +8,30 @@ import { userAuthRouter } from "./userRouter";
 
 const educationRouter = Router();
 
-educationRouter.get("/education", login_required, async (req,res) => {
-    const user_id = req.currentUserId;
-    // const user_id = "85be55c4-f540-439d-956d-273aa9db08ef";
-    const user = await userAuthService.getUserInfo({
-      user_id,
-    })
-    const educations = await educationService.getEducations({user});
-    res.json(educations);
+educationRouter.get("/education", login_required, async (req, res, next) => {
+
+  const user_id = req.currentUserId;
+  const user = await userAuthService.getUserInfo({
+    user_id,
+  });
+  const educations = await educationService.getEducations({ user });
+
+  res.json(educations);
+
 });
 
-educationRouter.post("/education", login_required, async (req,res)=> {
-  
+educationRouter.post("/education", login_required, async (req, res, next) => {
+  try {
+    if (is.emptyObject(req.body)) {
+      throw new Error(
+        "값이 비어있습니다."
+      );
+    }
     const user_id = req.currentUserId;
-    // const user_id ="85be55c4-f540-439d-956d-273aa9db08ef";
     const user = await userAuthService.getUserInfo({
       user_id,
-    })
-  
+    });
+
     const school = req.body.school;
     const major = req.body.major;
     const position = req.body.position;
@@ -37,24 +43,35 @@ educationRouter.post("/education", login_required, async (req,res)=> {
       major,
       position,
     });
-    res.json(newEducation);
+    if (newEducation.errorMessage) {
+      throw new Error(newEducation.errorMessage);
+    }
+
+    res.status(201).json(newEducation);
+  } catch (error) {
+    next(error);
+  }
 });
 
 
-educationRouter.put("/education/:id", login_required, async (req,res)=> {
-
+educationRouter.put("/education/:id", login_required, async (req, res, next) => {
+  try {
     const id = req.params.id;
     const school = req.body.school;
     const major = req.body.major;
     const position = req.body.position;
-    
+
     const newEducation = await educationService.updateEducation({
       id,
       school,
       major,
       position,
     });
+
     res.json(newEducation);
+  } catch (error) {
+    next(error);
+  }
 });
 
 
