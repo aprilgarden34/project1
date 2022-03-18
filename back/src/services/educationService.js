4fb11058fdf6e8c40bfc6d53e3917f999e78a770
GeonTae Park <rssr96@naver.com>
2022-03-18 18:03:06 +0900

    Fix: URI 수정

--- back/src/services/educationService.js
+++ back/src/services/educationService.js
@@ -1,24 +1,39 @@
 import { Education } from "../db"; // from을 폴더(db) 로 설정 시, 디폴트로 index.js 로부터 import함.
 import { v4 as uuidv4 } from "uuid";
+import parse from "nodemon/lib/cli/parse";
 
 
 class educationService {
     static async addEducation({ user, school, major, position }) {
 
         const id = uuidv4();
-        const newEducation = {user, id, school,major, position};
-        const education = await Education.create({newEducation});
-        return education;
+        const newEducation = { user, id, school, major, position };
+        const education = await Education.create({ newEducation });
+        return {
+            ...this.parseEducation({education}),
+            user_id:user.id
+        }
     }
-    static async updateEducation({ id, school, major, position}){
-        
-        const newEducation = {id, school, major, position};
+    static async updateEducation({ id, school, major, position }) {
+
+        const newEducation = { id, school, major, position };
         const education = await Education.update(newEducation);
-        return education;
+        return this.parseEducation({education});
     }
-    static async getEducations({user}){
-        const educations = await Education.findByUser({user});
+    static async getEducations({ user }) {
+        let educations = await Education.findByUser({ user });
+        educations = educations.map(education=>{
+            return this.parseEducation({education})
+        })
         return educations;
+    }
+
+    static parseEducation({ education }) {
+        return {
+            school: education.school,
+            major: education.major,
+            position: education.position
+        }
     }
 };
 
