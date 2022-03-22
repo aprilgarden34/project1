d478b9bab92634fec5b7a1e625ce3d94d415dfdc
minji_ <minjikim.sp@gmail.com>
2022-03-22 20:41:19 +0900

    Feat: 학력 삭제기능 추가

--- front/src/components/education/Education.js
+++ front/src/components/education/Education.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import EducationCard from "./EducationCard";
 import EducationEditForm from "./EducationEditForm";
 
-function Education({ portfolioOwnerId, education, setEducations, isEditable }) {
+function Education({ portfolioOwnerId, education, educations, setEducations, isEditable }) {
   //useState로 isEditing 상태를 생성함. 기본 값은 false로 설정
   const [isEditing, setIsEditing] = useState(false);
   return (
@@ -17,6 +17,8 @@ function Education({ portfolioOwnerId, education, setEducations, isEditable }) {
       ) : (
         <EducationCard
           education={education}
+          educations={educations}
+          setEducations={setEducations}
           isEditable={isEditable}
           setIsEditing={setIsEditing}
         />
