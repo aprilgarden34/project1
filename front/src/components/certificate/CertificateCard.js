b73aa5eb449d1b707184b8498ab12651dbcfda88
Heyden <nhm6947@gmail.com>
2022-03-25 01:00:52 +0900

    Feat: 자격증 업로드 POST, PATCH 구현 - 프론트엔드

--- front/src/components/certificate/CertificateCard.js
+++ front/src/components/certificate/CertificateCard.js
@@ -1,14 +1,37 @@
 import { Card, Button, Row, Col, Form } from "react-bootstrap";
-import React from "react";
+import React, { useState } from "react";
 import * as Api from "../../api";
 
 function CertificateCard({ certificates, setCertificates, certificate, isEditable, setIsEditing }) {
 
   //  -------- 파일 업로드; 데이터 세팅 ---------------
 
-  const handleChange = async (e) => {
+  const [certificateFilePath, setCertificateFilePath] = useState("");
 
+  // (프론트) filepath를 (백엔드) DB에 저장하는 handleSubmit 이벤트핸들러
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    // portfolioOwnerId를 currentUserId 변수에 할당함.
+    // const currentUserId = portfolioOwnerId;
+    const currentCertificateId = certificate.id;
+    console.log('certificate id:', certificate.id);
+
+    // 백엔드의 'certificate/saveFile' URI로 formPatch 요청 (참고. app.js 의 formPost)
+    const res = await Api.patch("certificate/saveFile", {
+      certificateId: currentCertificateId,
+      filePath: certificateFilePath
+    });
+    
+    console.log('저장된 값은 과연 ', res.data);
+    alert('백엔드에 이미지 파일이 저장되었습니다!')
+
+  };
+
+  // (프론트) 업로드 UI 내용물이 바뀔 때 (백엔드) uploads 폴더에 저장하는 handleChange 이벤트핸들러
+  const handleChange = async (e) => {
+    e.preventDefault();
+
 
     // files 데이터 전달할 formData 생성
     const formData = new FormData();
@@ -17,21 +40,21 @@ function CertificateCard({ certificates, setCertificates, certificate, isEditabl
     const uploadFile = e.target.files[0];
     formData.append("file", uploadFile);
 
-    // 백엔드의 'certificate/uploadfile' URI로 formPost 요청 (참고. app.js 의 formPost)
-    const res = await Api.formPost("upload", formData);
+    // 백엔드의 'certificate/uploadfile' URI로 formPatch 요청 (참고. app.js 의 formPost)
+    const res = await Api.filePost("certificate/uploadFile", formData);
 
     // 백엔드에서 에러 발생하는 경우, 예외처리
     if (res.data.error === false) {
-      alert('error : 이미지 형식 파일인지 확인해주세요!');
+      return alert('5mb 이하의 이미지 형식 파일인지 확인해주세요!');
     } else {
-
+      // 백엔드에서 에러가 발생하지 않는 경우, filePath를 CertificateFilePath에 할당
       const fileInfo = {
         filePath: res.data.path,
         fileName: res.data.filename
       }
       console.log('백엔드에 저장된 데이터: ',fileInfo);
-      alert('백엔드에 이미지 파일이 저장되었습니다!')
 
+      setCertificateFilePath(fileInfo.filePath);
     }   
   };
 
@@ -49,9 +72,12 @@ function CertificateCard({ certificates, setCertificates, certificate, isEditabl
             <br />
             <span className="text-muted">{certificate.certificateDesc}</span>
             <br />
-            <span className="text-muted">{certificate.certificateDate}</span>    
+            <span className="text-muted">{certificate.certificateDate}</span>   
+            <br />
+            <span className="text-muted">파일명.확장자</span> 
           </div>
           {/* // 파일 업로드  */}
+          <Form onSubmit={handleSubmit} encType="multipart/form-data" >
             <Form.Group controlId="formFileSm" className="mt-3">
               <Form.Control 
                 type="file" 
@@ -60,20 +86,29 @@ function CertificateCard({ certificates, setCertificates, certificate, isEditabl
                 onChange={handleChange}
                 />
             </Form.Group>
+            <Button
+                  variant="outline-info" 
+                  type="submit"
+                  size="sm"
+                  className="mr-3"
+                  
+                >업로드
+                </Button>
+          </Form>
           {/* // 파일 업로드  */}
         </Col>
         {isEditable && (
             <Col xs lg="1">
               <div className="d-grid gap-2">
                 <Button
-                  variant="outline-info"
+                  variant="outline-primary" 
                   size="sm"
                   onClick={() => setIsEditing((prev) => !prev)} //editing 모드를 편집상태로 전환!
                   className="mr-3"
                 >편집
                 </Button>
                 <Button
-                  variant="outline-info"
+                  variant="outline-danger"
                   size="sm"
                   className="mr-3"
                   onClick={() => {
