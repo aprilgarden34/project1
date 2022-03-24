01a0aa6ae9fd3113e1e8d6e8aff87346613041a7
minji_ <minjikim.sp@gmail.com>
2022-03-24 19:03:59 +0900

    Feat: user 업로드파일 기능 추가

--- front/src/components/user/UserCard.js
+++ front/src/components/user/UserCard.js
@@ -1,18 +1,46 @@
 import { useNavigate } from "react-router-dom";
-import { Card, Row, Button, Col } from "react-bootstrap";
+import { useState } from "react"
+import { Card, Row, Button, Col, Form } from "react-bootstrap";
+import * as Api from "../../api";
 
 function UserCard({ user, setIsEditing, isEditable, isNetwork }) {
   const navigate = useNavigate();
+  const [ userImage, setUserImage ] = useState(null)
+
+  //파일업로드시 작동
+  const SubmitHandler = async (e) => {       
+    e.preventDefault();
+       
+    const formData = new FormData();
+    formData.append("file", userImage);
+    const res = await Api.formPost("user/image", formData);
+
+    if (res.data) {
+      const fileInfo = {
+        filePath: res.data.path,
+        fileName: res.data.filename
+      }
+      console.log('백엔드에 저장된 데이터: ',fileInfo);      
+      alert('백엔드에 이미지 파일이 저장되었습니다!')
+    } 
+  };
+
   return (
     <Card className="mb-2 ms-3 mr-5" style={{ width: "18rem" }}>
       <Card.Body>
         <Row className="justify-content-md-center">
-          <Card.Img
-            style={{ width: "10rem", height: "8rem" }}
-            className="mb-3"
-            src="http://placekitten.com/200/200"
-            alt="랜덤 고양이 사진 (http://placekitten.com API 사용)"
-          />
+          <Form 
+            encType="multipart/form-data" 
+            style={{ width: "10rem",  height: "8rem" }}
+            onSubmit={SubmitHandler}
+            >  
+            <Form.Group controlId="formFile" className="mb-3">
+              <Form.Label>프로필 사진을 업로드해주세요.</Form.Label>            
+              <Form.Control type="file" onChange={(e) => setUserImage(e.target.files[0])} /> 
+              <Form.Control type="submit" />           
+            </Form.Group>
+          </Form>
+          
         </Row>
         <Card.Title>{user?.name}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">{user?.email}</Card.Subtitle>
@@ -23,7 +51,7 @@ function UserCard({ user, setIsEditing, isEditable, isNetwork }) {
             <Row className="mt-3 text-center text-info">
               <Col sm={{ span: 20 }}>
                 <Button
-                  variant="outline-info"
+                  variant="outline-primary"
                   size="sm"
                   onClick={() => setIsEditing(true)}
                 >
