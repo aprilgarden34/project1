05ecb481c7b5e9125fe69f73c7338216171f7e67
minji_ <minjikim.sp@gmail.com>
2022-03-25 16:49:53 +0900

    Feat: preview 추가

--- front/src/components/education/EducationCard.js
+++ front/src/components/education/EducationCard.js
@@ -4,7 +4,9 @@ import * as Api from "../../api";
 
 // 편집 버튼 활성화는 isEditable이 true, setIsEditing(true)일 경우만 가능
 function EducationCard({ educations, setEducations, education, isEditable, setIsEditing }) {
-  const [ educationImage, setEducationImage ] = useState(null)
+  
+  const [image, setImage] = useState({ preview: '', data: '' })
+  const [educationFilePath, setEducationFilePath] = useState(null);
 
   //삭제시 작동
   const DeleleteHandler = async (e) => {
@@ -21,23 +23,51 @@ function EducationCard({ educations, setEducations, education, isEditable, setIs
     });
   };
 
-  //파일업로드시 작동
-  const SubmitHandler = async (e) => {       
+   // (프론트) filepath를 (백엔드) DB에 저장하는 handleSubmit 이벤트핸들러
+   const handleSubmit = async (e) => {
     e.preventDefault();
-       
-    const formData = new FormData();
-    formData.append("file", educationImage);
-    const res = await Api.formPost("education/image", formData);
 
-    if (res.data) {
+    const currentEducationId = education.id;
+    console.log('education id:', education.id);
+
+    const res = await Api.patch("certificate/saveFile", {
+      userId: currentEducationId,
+      filePath: educationFilePath
+    });
+    
+    console.log('저장된 값은 ', res.data);
+    alert('백엔드에 이미지 파일이 저장되었습니다!')
+
+  };
+
+  // (프론트) 업로드 UI 내용물이 바뀔 때 (백엔드) uploads 폴더에 저장하는 handleChange 이벤트핸들러
+  const handleChange = async (e) => {
+    e.preventDefault();
+    const img = {
+      preview: URL.createObjectURL(e.target.files[0]),
+      data: e.target.files[0],
+    }
+    setImage(img)
+
+    const formData = new FormData();
+    const uploadFile = e.target.files[0];
+    formData.append("file", uploadFile);
+
+    const res = await Api.filePost("education/uploadFile", formData);
+
+    if (res.data.error === false) {
+      return alert('5mb 이하의 이미지 형식 파일인지 확인해주세요!');
+    } else {
       const fileInfo = {
         filePath: res.data.path,
         fileName: res.data.filename
       }
-      console.log('백엔드에 저장된 데이터: ',fileInfo);      
-      alert('백엔드에 이미지 파일이 저장되었습니다!')
-    } 
-  };
+      console.log('백엔드에 저장된 데이터: ',fileInfo);
+
+      setEducationFilePath(fileInfo.filePath);
+    }   
+  };    
+
   return (
     <Card.Text>
       <Row className="align-items-center">
@@ -45,11 +75,12 @@ function EducationCard({ educations, setEducations, education, isEditable, setIs
         <Form 
           encType="multipart/form-data" 
           style={{ display: 'flex' }}
-          onSubmit={SubmitHandler}
+          onSubmit={handleSubmit}
           >  
           <Form.Group controlId="formFile" className="mb-3">
-            <Form.Label>학력 파일을 업로드해주세요.</Form.Label>            
-            <Form.Control type="file" onChange={(e) => setEducationImage(e.target.files[0])} /> 
+            <Form.Label>학력 파일을 업로드해주세요.</Form.Label>
+            <Form>{image.preview && <img src={image.preview} alt="preview"width='100' height='100' />}</Form>            
+            <Form.Control type="file" onChange={handleChange} /> 
             <Form.Control type="submit" />           
           </Form.Group>
         </Form>
